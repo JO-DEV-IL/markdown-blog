35f276fbc2957b22a2f8b35ae413ec79fe2fcf8a
JO-DEV-IL <jillianokeefe.dev@gmail.com>
2022-10-06 19:08:16 -0400

    server setup cont

---
+++ routes/articles.js
@@ -0,0 +1,12 @@
+// This is where all the routes directly related to articles will go
+
+// Requirements //
+const express = require('express')
+
+// Router will allow for the same functionality as app.get rendering
+// Keeps routes clean and organized to prevent a large server.js file
+// by giving them their own file
+const router = express.Router()
+
+// Exports router file to server.js dependencies
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-// SERVER REQUIREMENTS
+// DEPENDENCIES //
 const express = require('express')
 const app = express()
+const articleRouter = require('./routes/articles')
 
 //Server Port
-app.listen(8000)
+const port = 8000
+app.listen(port)
 
+//Server response to test OK
+//Renders index.ejs file to localhost:port
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+//View engine will convert ejs code into html
+app.set('view engine', 'ejs')
