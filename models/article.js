38d0e05440e429778c43792e4fa5a3239fca49fc
JO-DEV-IL <jillianokeefe.dev@gmail.com>
2022-10-09 10:54:02 -0400

    Update article.js

--- models/article.js
+++ models/article.js
@@ -3,7 +3,7 @@
 //MongoDB access
 const mongoose = require('mongoose')
 
-//create markdown and convert into HTML
+//library to create markdown and convert into HTML
 const marked = require('marked')
 
 //Library to create 'slugs' (version of title to use as url path)
@@ -26,6 +26,20 @@ const articleSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+//Anytime anything is done to an article, if-conditional function runs using pre() parameter
+//if there is an article title, slugify said article title
+//give it properties of lowercase (lower) and any characters that dont fit into a url is removed (strict)
+articleSchema.pre('validate', function(){
+    if(this.title){
+        this.slug = slugify(this.title, { lower: true, strict: true })
     }
 })
 
